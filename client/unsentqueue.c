2729723f6c75556c35cba0d8834daadf4a4df655
EmmanuelMess <emmanuelbendavid@gmail.com>
2021-05-17 12:58:41 -0300

    Fix crash on /exit

--- client/unsentqueue.c
+++ client/unsentqueue.c
@@ -1,18 +1,25 @@
 #include <malloc.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdatomic.h>
 #include "unsentqueue.h"
+#include "errormanagement.h"
 
 TAILQ_HEAD(unsent_messages_queue, unsent_message);// Se declara unsents del tipo tailHead
 static struct unsent_messages_queue unsents = {0};
 static pthread_mutex_t queueLock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t conditionNotEmpty = PTHREAD_COND_INITIALIZER;
+static atomic_bool isDestroyed = false;
 
 void queueOutboundInit() {
 	TAILQ_INIT(&unsents);
 }
 
 void queueOutboundDestroy() {
+	if(isDestroyed) {
+		return;
+	}
+
 	pthread_mutex_lock(&queueLock);
 	while (!TAILQ_EMPTY(&unsents)) {
 		struct unsent_message* e = TAILQ_FIRST(&unsents);
@@ -21,9 +28,15 @@ void queueOutboundDestroy() {
 	}
 	pthread_mutex_unlock(&queueLock);
 	pthread_mutex_destroy(&queueLock);
+
+	isDestroyed = true;
 }
 
 void queueOutboundInsert(struct message outboundMessage) {
+	if(isDestroyed) {
+		return;
+	}
+
 	struct unsent_message* message = malloc(sizeof(struct unsent_message));
 	memcpy(&(message->message), &outboundMessage, sizeof (struct message));
 
@@ -34,6 +47,11 @@ void queueOutboundInsert(struct message outboundMessage) {
 }
 
 struct unsent_message queueOutboundPeek() {
+	if(isDestroyed) {
+		perror("Peek de la cola de salida destruida");
+		EXIT_ON_FALUIRE(-1);
+	}
+
 	pthread_mutex_lock(&queueLock);
 	while(TAILQ_EMPTY(&unsents)) {
 		pthread_cond_wait(&conditionNotEmpty, &queueLock);
@@ -45,6 +63,10 @@ struct unsent_message queueOutboundPeek() {
 }
 
 void queueOutboundRemove() {
+	if(isDestroyed) {
+		return;
+	}
+
 	pthread_mutex_lock(&queueLock);
 	struct unsent_message *message = TAILQ_FIRST(&unsents);
 	TAILQ_REMOVE(&unsents, message, next);
